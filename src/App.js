97aeb243fb1bf01f513952b4f7120571fe402548
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-03-24 20:38:30 +0530

    test cases using Jest library in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-5'>
+      <form>
+        <div className='mb-3'>
+          <label htmlFor='email'>Email address</label>
+          <input
+            type='email'
+            placeholder='enter email'
+            className='form-control'
+            id='email'
+            name='email'
+          />
+        </div>
+        <div>
+          <label htmlFor='password'></label>
+        </div>
+      </form>
     </div>
   );
 }
